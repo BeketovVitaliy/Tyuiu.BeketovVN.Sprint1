014ec163a7801f3834e9a1fab5ca6c3ece000bb1
Avanor <avanor5242@yandex.ru>
2023-09-21 23:39:03 +0500

    Task3

---
+++ Tyuiu.BeketovVN.Sprint1.Task3.V11.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.BeketovVN.Sprint1.Task3.V11.Lib
+{
+    public class DataService : ISprint1Task3V11
+    {
+        public double TriangleArea(double x1, double x2, double x3, double y1, double y2, double y3)
+        {
+            double S;
+            S = 0.5 * (x1 * (y2 - y3) +x2 * (y3 - y1) + x3 * (y1 - y2));
+
+            return (Math.Abs(S));
+        }
+    }
+}
+//Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты и печатает результат на экране.
+//Расчеты:
+//Напишите программу, которая вычисляет площадь треугольника, если известны координаты его углов.
+//Введите координаты углов (числа разделяйте пробелом):
+
+//x1-> - 2
+
+//y1-> 5
+
+//х2-> 1
+
+//у2-> 7
+
+//хЗ-> 5
+
+//уЗ-> - 3
+
+//Площадь треугольника: 19 кв.см
---
+++ Tyuiu.BeketovVN.Sprint1.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BeketovVN.Sprint1.Task3.V11.Lib;
+
+namespace Tyuiu.BeketovVN.Sprint1.Task3.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x1 = -2, x2 = 1, x3 = 5, y1 = 5, y2 = 7, y3 = -3;
+            var res = ds.TriangleArea(x1,x2,x3,y1,y2,y3);
+            Assert.AreEqual(19, res);
+        }
+    }
+}
+//Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты и печатает результат на экране.
+//Расчеты:
+//Напишите программу, которая вычисляет площадь треугольника, если известны координаты его углов.
+//Введите координаты углов (числа разделяйте пробелом):
+
+//x1-> - 2
+
+//y1-> 5
+
+//х2-> 1
+
+//у2-> 7
+
+//хЗ-> 5
+
+//уЗ-> - 3
+
+//Площадь треугольника: 19 кв.см
